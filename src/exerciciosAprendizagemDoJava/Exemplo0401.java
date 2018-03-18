f688116c2c922a5c77d860b459f5651e8715d647
avsouza.es@gmail.com <avsouza.es@gmail.com>
2018-03-18 14:20:19 -0300

    Atualizando arquivos de exercicios

---
+++ src/exerciciosAprendizagemDoJava/Exemplo0401.java
@@ -0,0 +1,13 @@
+package exerciciosAprendizagemDoJava;
+
+public class Exemplo0401 {
+	
+	public static void main(String[] args) {
+	
+		double A=5.2,B=5.6,C=-5.8;
+		
+		System.out.println("Arredondado 5.2 = " + Math.ceil(A));
+		System.out.println("Arredondado 5.6 = " + Math.ceil(B));
+		System.out.println("Arredondado -5.8 = " + Math.ceil(C));
+	}	
+}
