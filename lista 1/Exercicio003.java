5dfe2724ef65e2168bbdb2d319649061d5d6b6b0
Alexandre Viera de Souza <avsouza.es@gmail.com>
2019-06-09 16:30:14 -0300

    Adicionando Exercicio003 ao repositorio.

--- lista 1/Exercicio003.java
+++ lista 1/Exercicio003.java
@@ -7,3 +7,31 @@
  * 		- Preço unitário da peça
  * 		- Quantidade vendida
 *************************************************************************/
+
+import java.util.Scanner;
+
+class Exercicio003{
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		//declaracao de variaveis
+		int idVendedor, idPeca;
+		float totalVenda, precoUnitarioPeca, quantidadeVendida, comissao=0.5f;
+		double valorPagarComissao; 
+		
+		System.out.print("Digite o código do vendedor: ");
+		idVendedor = entrada.nextInt();
+		System.out.print("Digite o código da Peça: ");
+		idPeca = entrada.nextInt();
+		System.out.print("Digite o preço unitário da peça: ");
+		precoUnitarioPeca = entrada.nextFloat();
+		System.out.print("Digite o quantidade de peças vendidas: ");
+		quantidadeVendida = entrada.nextFloat();
+		
+		valorPagarComissao = (precoUnitarioPeca * quantidadeVendida) * comissao;
+		totalVenda = (precoUnitarioPeca * quantidadeVendida);
+		System.out.print("Total da venda e de R$" +totalVenda +"\n");
+		System.out.print("A comissa a ser paga é de  R$" +valorPagarComissao +"\n");
+	}
+}
